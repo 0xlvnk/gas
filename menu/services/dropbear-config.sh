30e9469452c786ba305910bc5606697ea3fa6893
0xlvnk <kalvinaditya205@gmail.com>
2025-06-19 15:15:52 +0700

    Add files via upload

---
+++ menu/services/dropbear-config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cat > /etc/default/dropbear << EOF
+NO_START=0
+DROPBEAR_PORT=109
+DROPBEAR_EXTRA_ARGS="-p 127.0.0.1:109 -p 127.0.0.1:143 -p 127.0.0.1:69 -p 127.0.0.1:222 -p 127.0.0.1:777"
+EOF
+
+systemctl restart dropbear
